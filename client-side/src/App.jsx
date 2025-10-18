3d7bad2462221a0fe88a1f2f606719d506b11752
darkk1ngt <noellusale7@gmail.com>
2025-10-18 16:00:21 +0100

    "Added an about and contact page"

--- client-side/src/App.jsx
+++ client-side/src/App.jsx
@@ -5,6 +5,7 @@ import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import Login from "./pages/Login";
 import Register from "./pages/Register";
+import About from "./pages/About";
 import Logout from "./pages/Logout";
 import Forgot from "./pages/Forgot";
 import Reset from "./pages/Reset";
