cc4b4940c7c30d04b9f7b7ab2553bc367dccaf50
darkk1ngt <noellusale7@gmail.com>
2026-01-14 09:49:46 +0000

    remake

--- server/src/database/index.ts
+++
@@ -1,5 +0,0 @@
-import { initializeDatabase } from "./setup.js";
-import * as User from "./models/User.js";
-import * as Booking from "./models/Booking.js";
-
-export { initializeDatabase, User, Booking };
