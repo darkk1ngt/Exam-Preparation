7825fbfbf12de5ba97a9e131519f9955726b0113
darkk1ngt <noellusale7@gmail.com>
2026-03-11 11:04:32 +0000

    got rid of da one

--- TASK 2/da one one/Client-Side/src/instructional.jsx
+++
@@ -1,9 +0,0 @@
-// 1. Install axios to let api requests go to and from the backend
-// 2. Main ideas are 
-// - components (re-usable data)
-// - states(data that changes)
-// - effects(what happens on component loading)
-// 3.Login using states,forms and API calls
-// 4.Register page 
-// 5.Auth Context for consistent user login across all pages ( updating login and registration endpoints )
-// 6. React-Router-Dom for individual page routing and added a protected pathway  
