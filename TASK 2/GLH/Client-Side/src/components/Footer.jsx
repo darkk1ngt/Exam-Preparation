270cb67cc04c374cda7f26da5860b516afdfcbe0
darkk1ngt <noellusale7@gmail.com>
2026-03-17 04:35:20 +0000

    fuck

--- TASK 2/GLH/Client-Side/src/components/Footer.jsx
+++
@@ -1,8 +0,0 @@
-export default function Footer(){
-    return(
-        <footer className="footer">
-            <p>© 2026 Greenfield Local Hub</p>
-            <p>Local producers. Fresh local food. Community-first fulfilment.</p>
-        </footer>
-    )
-}
